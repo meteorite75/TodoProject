4623f87eed8829730ec4d2613a5662def024201f
shahab <razavi.shahab@yahoo.com>
2024-01-03 15:00:54 +0330

    method 'POST' added to the api_view

--- todo/serializers.py
+++ todo/serializers.py
@@ -5,4 +5,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ['id', 'titel', 'content']
+        fields = '__all__'
